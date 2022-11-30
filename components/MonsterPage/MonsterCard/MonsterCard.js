d19b4172a48f0bdbd92ec8b0485e4d72ec6f32ff
Marcus Enblom <marcusenblom@gmail.com>
2022-11-30 11:53:19 +0100

    card design

--- components/MonsterPage/MonsterCard/MonsterCard.js
+++ components/MonsterPage/MonsterCard/MonsterCard.js
@@ -7,8 +7,15 @@ export default function MonsterCard({ monster, index }){
         <li className={styles.monster}>
 
             <div className={styles.card}>
+                <div className={styles.bgLayerOne}>
+                    <img src="/img/card/background/card_bg_metal.png" alt="" />
+                </div>
+                <div className={styles.bgLayerTwo}>
+                    <img src="/img/card/background/card_bg_monster.jpg" alt="" />
+                </div>
 
-                <div className={styles.iconContainer}>
+                <div className={styles.typeContainer}>
+                    <div className={styles.bg}></div>
                     <div className={styles.icon}>
                         <img src={`/img/placeholders/${monster?.type || "beast"}.jpg`} alt="" />
                     </div>
