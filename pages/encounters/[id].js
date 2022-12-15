b9f8a85580fe3d8b4a43ed2c605d05fbaf282fb2
Marcus Enblom <marcusenblom@gmail.com>
2022-12-15 18:55:31 +0100

    encounters and encounter page

--- pages/encounters/[id].js
+++ pages/encounters/[id].js
@@ -1,4 +1,4 @@
-import EncounterPage from "components/EncounterPage/EncounterPage";
+import EncounterPage from "components/EncountersPage/EncounterPage";
 import Link from "next/link";
 
 export default function Index({}) {
---
+++ pages/encounters/index.js
@@ -0,0 +1,9 @@
+import EncounterPage from "components/EncountersPage/EncounterPage";
+import Link from "next/link";
+
+export default function Index({}) {
+
+  return (
+    <EncounterPage />
+  )
+}
